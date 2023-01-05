02bceb7a4e11db347bd4b4e7879e00c0f7dc831b
arathi <you@example.com>
2023-01-05 11:11:13 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+}
+
+func allUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/gorm_users?charset=utf8&parseTime=True")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	var users []User
+	db.Find(&users)
+	fmt.Println("{}", users)
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func newUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("New User Endpoint Hit")
+
+	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/gorm_users?charset=utf8&parseTime=True")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+
+	fmt.Println(name)
+	fmt.Println(email)
+
+	db.Create(&User{Name: name, Email: email})
+	fmt.Fprintf(w, "New User Successfully Created")
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/gorm_users?charset=utf8&parseTime=True")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var user User
+	db.Where("id = ?", id).Find(&user)
+	db.Delete(&user)
+
+	fmt.Fprintf(w, "Successfully Deleted User")
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/gorm_users?charset=utf8&parseTime=True")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+
+	var user User
+	db.Where("name = ?", name).Find(&user)
+
+	user.Email = email
+
+	db.Save(&user)
+	fmt.Fprintf(w, "Successfully Updated User")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/users", allUsers).Methods("GET")
+	myRouter.HandleFunc("/user/{id}", deleteUser).Methods("DELETE")
+	myRouter.HandleFunc("/user/{name}/{email}", updateUser).Methods("PUT")
+	myRouter.HandleFunc("/user/{name}/{email}", newUser).Methods("POST")
+	log.Println(fmt.Sprintf("Starting Server on port %s", ":8081"))
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
+func initialMigration() {
+	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/gorm_users?charset=utf8&parseTime=True")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+}
+
+func main() {
+	fmt.Println("Go ORM Tutorial")
+
+	initialMigration()
+	// Handle Subsequent requests
+	handleRequests()
+}
